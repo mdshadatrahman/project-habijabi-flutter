15fd4d58da2e669ec8a53a04c0011ef705a85834
Shadat Rahman <shadat.rahman.464@gmail.com>
2023-09-29 22:19:42 +0600

    init

---
+++ android/app/src/main/kotlin/com/shadatraman/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.shadatraman.project_habijabi_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:project_habijabi_flutter/app/app.dart';
+import 'package:project_habijabi_flutter/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
